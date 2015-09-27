45ae7ec0fb2dfcfce17351a4ba9f210c71675212
Nathan Spielman <nathan.spielman@gmail.com>
2015-09-27 12:54:41 -0400

    Fix broken articles controller tests
    This commit fixes failing articles controller tests by running a before
    each block that stubs the user sessions validation so that all tests
    believe that the user is properly signed in.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -31,15 +31,14 @@ RSpec.describe ArticlesController, type: :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ArticlesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  before(:each) do
+    allow(controller).to receive(:signed_in?).and_return(true)
+  end
 
   describe "GET #index" do
     it "assigns all articles as @articles" do
       article = Article.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, {}
       expect(assigns(:articles)).to eq([article])
     end
   end
@@ -47,14 +46,14 @@ RSpec.describe ArticlesController, type: :controller do
   describe "GET #show" do
     it "assigns the requested article as @article" do
       article = Article.create! valid_attributes
-      get :show, {:id => article.to_param}, valid_session
+      get :show, {:id => article.to_param}
       expect(assigns(:article)).to eq(article)
     end
   end
 
   describe "GET #new" do
     it "assigns a new article as @article" do
-      get :new, {}, valid_session
+      get :new, {}
       expect(assigns(:article)).to be_a_new(Article)
     end
   end
@@ -62,7 +61,7 @@ RSpec.describe ArticlesController, type: :controller do
   describe "GET #edit" do
     it "assigns the requested article as @article" do
       article = Article.create! valid_attributes
-      get :edit, {:id => article.to_param}, valid_session
+      get :edit, {:id => article.to_param}
       expect(assigns(:article)).to eq(article)
     end
   end
@@ -71,30 +70,30 @@ RSpec.describe ArticlesController, type: :controller do
     context "with valid params" do
       it "creates a new Article" do
         expect {
-          post :create, {:article => valid_attributes}, valid_session
+          post :create, {:article => valid_attributes}
         }.to change(Article, :count).by(1)
       end
 
       it "assigns a newly created article as @article" do
-        post :create, {:article => valid_attributes}, valid_session
+        post :create, {:article => valid_attributes}
         expect(assigns(:article)).to be_a(Article)
         expect(assigns(:article)).to be_persisted
       end
 
       it "redirects to the created article" do
-        post :create, {:article => valid_attributes}, valid_session
+        post :create, {:article => valid_attributes}
         expect(response).to redirect_to(Article.last)
       end
     end
 
     context "with invalid params" do
       it "assigns a newly created but unsaved article as @article" do
-        post :create, {:article => invalid_attributes}, valid_session
+        post :create, {:article => invalid_attributes}
         expect(assigns(:article)).to be_a_new(Article)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {:article => invalid_attributes}, valid_session
+        post :create, {:article => invalid_attributes}
         expect(response).to render_template("new")
       end
     end
@@ -108,20 +107,20 @@ RSpec.describe ArticlesController, type: :controller do
 
       it "updates the requested article" do
         article = Article.create! valid_attributes
-        put :update, {:id => article.to_param, :article => new_attributes}, valid_session
+        put :update, {:id => article.to_param, :article => new_attributes}
         article.reload
         skip("Add assertions for updated state")
       end
 
       it "assigns the requested article as @article" do
         article = Article.create! valid_attributes
-        put :update, {:id => article.to_param, :article => valid_attributes}, valid_session
+        put :update, {:id => article.to_param, :article => valid_attributes}
         expect(assigns(:article)).to eq(article)
       end
 
       it "redirects to the article" do
         article = Article.create! valid_attributes
-        put :update, {:id => article.to_param, :article => valid_attributes}, valid_session
+        put :update, {:id => article.to_param, :article => valid_attributes}
         expect(response).to redirect_to(article)
       end
     end
@@ -129,13 +128,13 @@ RSpec.describe ArticlesController, type: :controller do
     context "with invalid params" do
       it "assigns the article as @article" do
         article = Article.create! valid_attributes
-        put :update, {:id => article.to_param, :article => invalid_attributes}, valid_session
+        put :update, {:id => article.to_param, :article => invalid_attributes}
         expect(assigns(:article)).to eq(article)
       end
 
       it "re-renders the 'edit' template" do
         article = Article.create! valid_attributes
-        put :update, {:id => article.to_param, :article => invalid_attributes}, valid_session
+        put :update, {:id => article.to_param, :article => invalid_attributes}
         expect(response).to render_template("edit")
       end
     end
@@ -145,13 +144,13 @@ RSpec.describe ArticlesController, type: :controller do
     it "destroys the requested article" do
       article = Article.create! valid_attributes
       expect {
-        delete :destroy, {:id => article.to_param}, valid_session
+        delete :destroy, {:id => article.to_param}
       }.to change(Article, :count).by(-1)
     end
 
     it "redirects to the articles list" do
       article = Article.create! valid_attributes
-      delete :destroy, {:id => article.to_param}, valid_session
+      delete :destroy, {:id => article.to_param}
       expect(response).to redirect_to(articles_url)
     end
   end
